05935a45c375bc6425f47c95aaf939130d7c80b5
UggaBuga <urrgar@gmail.com>
2020-05-27 21:20:07 +0500

    Update main.cpp
    dfgfgsf

--- main.cpp
+++ main.cpp
@@ -5,6 +5,13 @@ int main()
 {
 	//setlocale(0, "Russian");
 	setlocale(LC_ALL, "ru");
-	cout << "Привет Кириллу и Мефодию"<<endl;
+	std::cout << "Привет Кириллу и Мефодию"<<std::endl;
 
+	cout << "Escape-последовательность \n";
+	cout << "Ещё строка \\ \' \" \n";
+
+	bool a = 5==2;
+	cout << a<< endl;
+
+	cin << a;
 }
